f1f01f5f55768ff6797657feb9eea6d67fa11ec1
唐亮 <tang97155@163.com>
2021-06-18 16:50:50 +0800

    Initial commit

---
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -0,0 +1,13 @@
+package com.newangels.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newangels/gen/GenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newangels.gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
