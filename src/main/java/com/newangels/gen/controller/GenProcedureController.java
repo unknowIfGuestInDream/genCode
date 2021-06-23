e8cbaf7033ea793a5d075592a4627e07820dc967
唐亮 <tang97155@163.com>
2021-06-23 09:31:35 +0800

    feat(新增生成代码下载功能):

--- src/main/java/com/newangels/gen/controller/GenProcedureController.java
+++ src/main/java/com/newangels/gen/controller/GenProcedureController.java
@@ -13,6 +13,7 @@ import com.newangels.gen.service.DataBaseProcedureService;
 import com.newangels.gen.service.GenProcedureModelService;
 import com.newangels.gen.service.NameConventService;
 import com.newangels.gen.util.DBUtil;
+import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +21,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * 存储过程代码生成
@@ -127,5 +133,78 @@ public class GenProcedureController {
         DataBaseProcedureService dbProcedure = DataBaseFactory.getDataBaseProcedure(DataBaseType.fromTypeName(driver));
 
         return BaseUtils.success(genProcedureModel.genCode(moduleName, packageName, userName, procedureNameList, nameConvent, dbProcedure, dbUtil));
+    }
+
+    /**
+     * 生成代码下载
+     */
+    @GetMapping(value = "downloadCode")
+    @Log
+    public void downloadRiderList(String moduleName, String genProcedureModelType, String nameConventType, String packageName, String url, String driver, String userName, String password, @RequestParam("procedureNameList") List<String> procedureNameList, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String zipName = moduleName + ".zip";
+            moduleName = BaseUtils.toUpperCase4Index(moduleName);
+            //获取数据库连接，为空则创建
+            DBUtil dbUtil = DbUtilsFactory.getDbUtil(url);
+            if (dbUtil == null) {
+                dbUtil = (DBUtil) DBUtil.getDbUtil().clone();
+                dbUtil.init(driver, url, userName, password);
+                DbUtilsFactory.register(url, dbUtil);
+            }
+            response.reset();
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/x-msdownload");
+            response.setHeader("Content-Disposition", "attachment;filename=" + BaseUtils.getFormatString(request, zipName));
+
+            ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
+            //获取生成代码模版
+            GenProcedureModelService genProcedureModel = GenProcedureModelFactory.getGenProcedureModel(GenProcedureModelType.fromTypeName(genProcedureModelType));
+            //获取命名规范
+            NameConventService nameConvent = NameConventFactory.getNameConvent(NameConventType.fromTypeName(nameConventType));
+            //获取数据库过程sql
+            DataBaseProcedureService dbProcedure = DataBaseFactory.getDataBaseProcedure(DataBaseType.fromTypeName(driver));
+            Map<String, Object> map = genProcedureModel.genCode(moduleName, packageName, userName, procedureNameList, nameConvent, dbProcedure, dbUtil);
+            List<String> list = (List<String>) map.get("list");
+
+            for (int i = 0, length = list.size(); i < length; i++) {
+                String name = list.get(i);
+                String fileName;
+                if ("BaseUtils".equals(name) || "ProcedureUtils".equals(name)) {
+                    fileName = name;
+                } else {
+                    fileName = moduleName + BaseUtils.toUpperCase4Index(name);
+                }
+                @Cleanup InputStream inputStream = new ByteArrayInputStream(name.getBytes("UTF-8"));
+                //创建输入流读取文件
+                @Cleanup BufferedInputStream bis = new BufferedInputStream(inputStream);
+                //将文件写入zip内，即将文件进行打包
+                zos.putNextEntry(new ZipEntry(fileName));
+                //写入文件的方法，同上
+                int size = 0;
+                byte[] buffer = new byte[4096];
+                //设置读取数据缓存大小
+                while ((size = bis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, size);
+                }
+                //关闭输入输出流
+                zos.closeEntry();
+            }
+            zos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setContentType("text/html;charset=utf-8");
+            @Cleanup PrintWriter out = null;
+            try {
+                out = response.getWriter();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            out.print("<span style=\"display:block;text-align: center;margin:0 auto;min-width: 150px;\">" + e.getMessage() + "</span><br/>");
+            out.print("<br/><button autocomplete=\"off\" onclick=\"javascript:window.history.back(-1);return false;\" autofocus=\"true\"\n" +
+                    "            style=\"display:block;margin:0 auto;min-width: 150px;background-color:rgb(0, 138, 203);color: rgb(255, 255, 255);\">\n" +
+                    "        返回上一个页面\n" +
+                    "    </button>");
+            out.flush();
+        }
     }
 }
