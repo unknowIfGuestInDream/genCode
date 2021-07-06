5b3e9fb231902265eeffa13aa906c6e82040731d
唐亮 <tang97155@163.com>
2021-07-06 11:18:21 +0800

    fix(sql时间参数修改):

--- src/main/java/com/newangels/gen/service/impl/DataBaseInfoServiceImpl.java
+++ src/main/java/com/newangels/gen/service/impl/DataBaseInfoServiceImpl.java
@@ -33,12 +33,12 @@ public class DataBaseInfoServiceImpl implements DataBaseInfoService {
 
     @Override
     public int insertDataBaseInfo(String NAME, String URL, String DRIVER, String USERNAME, String PASSWORD) {
-        return genJdbcTemplate.update("insert into database_info(NAME, URL, DRIVER, USERNAME, PASSWORD, UPDATE_TIME, CREATE_TIME) values(?, ?, ?, ?, ?, sysdate, sysdate)", NAME, URL, DRIVER, USERNAME, PASSWORD);
+        return genJdbcTemplate.update("insert into database_info(NAME, URL, DRIVER, USERNAME, PASSWORD, UPDATE_TIME, CREATE_TIME) values(?, ?, ?, ?, ?, sysdate(), sysdate())", NAME, URL, DRIVER, USERNAME, PASSWORD);
     }
 
     @Override
     public int updateDataBaseInfo(String ID, String NAME, String URL, String DRIVER, String USERNAME, String PASSWORD) {
-        return genJdbcTemplate.update("update database_info set NAME = ?, URL= ?, DRIVER = ?, USERNAME = ?, PASSWORD = ?, UPDATE_TIME = sysdate where ID = ?", NAME, URL, DRIVER, USERNAME, PASSWORD, ID);
+        return genJdbcTemplate.update("update database_info set NAME = ?, URL= ?, DRIVER = ?, USERNAME = ?, PASSWORD = ?, UPDATE_TIME = sysdate() where ID = ?", NAME, URL, DRIVER, USERNAME, PASSWORD, ID);
     }
 
     @Override
