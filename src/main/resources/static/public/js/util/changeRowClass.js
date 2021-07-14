da1cc9927e014e8d2527f69daf00666fb5eb1212
唐亮 <tang97155@163.com>
2021-07-14 15:27:54 +0800

    feat(changeRowClass.js新增方法):

--- src/main/resources/static/public/js/util/changeRowClass.js
+++ src/main/resources/static/public/js/util/changeRowClass.js
@@ -7,32 +7,15 @@
  * @param store
  * @returns {string}
  */
-function changeRowClass(record, rowIndex, rowParams, store) {
+function _changeRowClass(record, rowIndex, rowParams, store) {
     if (record.get('字段') === '') {
         return "x-grid-record-yellow";
     }
 }
 
-//各单位月预算上报流程预算管理办公室(成本)处理页，下发Panel
-function changeIssueRowClass(record, rowIndex, rowParams, store) {
-    if (record.get('V_FLAG') != '未下发') {
-        return "x-grid-record-gray";
-    }
-}
-
-//月上报科目编辑列表
-function changeBudgetMonthReportRowClass(record, rowIndex, rowParams, store) {
-    if (record.get('V_INPUT') == '否') {
-        return "x-grid-record-white";
-    }
-}
-
-//月上报单位列表
-function changeBudgetMonthOrgRowClass(record, rowIndex, rowParams, store) {
-    if (record.get('V_FLAG') == '已发起') {
-        return "x-grid-record-green";
-    } else if (record.get('V_FLAG') == '未发起') {
+//未编译的过程加背景色
+function _changeInValidRowClass(record, rowIndex, rowParams, store) {
+    if (record.get('STATUS') === 'INVALID') {
         return "x-grid-record-red";
     }
-
 }
