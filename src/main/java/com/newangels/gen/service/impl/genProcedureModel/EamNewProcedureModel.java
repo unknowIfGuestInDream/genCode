b8fe5f290341eeae5958577d1f1d1bfcd24fe3f3
唐亮 <tang97155@163.com>
2021-09-08 21:37:15 +0800

    fix(Eam3期规范重写dealProcdure方法以适应ant design分页命名规范):

--- src/main/java/com/newangels/gen/service/impl/genProcedureModel/EamNewProcedureModel.java
+++ src/main/java/com/newangels/gen/service/impl/genProcedureModel/EamNewProcedureModel.java
@@ -3,9 +3,15 @@ package com.newangels.gen.service.impl.genProcedureModel;
 import com.newangels.gen.enums.GenProcedureModelType;
 import com.newangels.gen.factory.AbstractGenProcedureModelFactory;
 import com.newangels.gen.service.AbstractGenProcedureModel;
+import com.newangels.gen.service.DataBaseProcedureService;
 import com.newangels.gen.service.NameConventService;
+import com.newangels.gen.util.DataSourceUtil;
+import freemarker.template.Configuration;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Eam3期风格代码
@@ -19,6 +25,17 @@ public class EamNewProcedureModel extends AbstractGenProcedureModel implements I
     @Override
     protected String getFtlPackageName() {
         return "eamNew";
+    }
+
+    @Override
+    protected Map<String, Object> dealProcdure(String moduleName, String packageName, String userName, List<String> procedureNameList, NameConventService nameConvent, DataBaseProcedureService dbProcedure, DataSourceUtil dataSourceUtil, Configuration configuration) {
+        Map<String, Object> result = super.dealProcdure(moduleName, packageName, userName, procedureNameList, nameConvent, dbProcedure, dataSourceUtil, configuration);
+        //ant design pro规范
+        String controllerMethod = result.get("controllerMethod").toString()
+                .replace("V_PAGESIZE", "pageSize")
+                .replace("V_PAGE", "current");
+        result.replace("controllerMethod", controllerMethod);
+        return result;
     }
 
     @Override
