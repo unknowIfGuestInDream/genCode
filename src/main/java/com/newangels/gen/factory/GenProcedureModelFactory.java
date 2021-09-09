d93d5161c43799f70b6a1ab1a8ab789f7c90cf13
唐亮 <tang97155@163.com>
2021-09-09 16:12:18 +0800

    fix(genProcedure和下载接口重写，原代码新增去除标识):

--- src/main/java/com/newangels/gen/factory/GenProcedureModelFactory.java
+++ src/main/java/com/newangels/gen/factory/GenProcedureModelFactory.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date: 2021/6/20 10:11
  * @since: 1.0
  */
+@Deprecated
 public class GenProcedureModelFactory {
     private static Map<GenProcedureModelType, GenProcedureModelService> strategyMap = new ConcurrentHashMap<>();
 
