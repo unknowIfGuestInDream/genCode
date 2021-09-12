0537d48033f10140f94cd1b3247a09a0c2df0f71
唐亮 <tang97155@163.com>
2021-09-12 12:37:51 +0800

    feat(命名规范预重构代码初步提交):

---
+++ src/main/java/com/newangels/gen/service/AbstractNameConvent.java
@@ -0,0 +1,13 @@
+package com.newangels.gen.service;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * 命名规范
+ *
+ * @author: TangLiang
+ * @date: 2021/9/11 23:29
+ * @since: 1.0
+ */
+public abstract class AbstractNameConvent implements InitializingBean {
+}
