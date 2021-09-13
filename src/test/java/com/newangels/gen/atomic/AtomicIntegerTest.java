8a36bcd9326d3f76fa5428a74d94d9eba6a8abba
唐亮 <tang97155@163.com>
2021-09-13 14:39:55 +0800

    test(新增AtomicIntegerTest测试类):

---
+++ src/test/java/com/newangels/gen/atomic/AtomicIntegerTest.java
@@ -0,0 +1,32 @@
+package com.newangels.gen.atomic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * AtomicInteger测试
+ *
+ * @author: TangLiang
+ * @date: 2021/9/13 14:15
+ * @since: 1.0
+ */
+@SpringBootTest
+public class AtomicIntegerTest {
+
+    @Test
+    public void test1() {
+        Map<String, AtomicInteger> map = new HashMap<>();
+        for (int i = 0; i < 3; i++) {
+            if (map.get("sss") == null) {
+                map.put("sss", new AtomicInteger(0));
+                continue;
+            }
+            System.out.println(map.get("sss").incrementAndGet());
+        }
+    }
+
+}
