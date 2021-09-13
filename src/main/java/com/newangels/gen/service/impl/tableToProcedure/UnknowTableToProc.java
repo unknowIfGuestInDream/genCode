c8b13d8977f67cae6c87ab87e145e9fa93b8564f
唐亮 <tang97155@163.com>
2021-09-13 20:33:31 +0800

    feat(表生成自增主键新增未知数据源处理):

---
+++ src/main/java/com/newangels/gen/service/impl/tableToProcedure/UnknowTableToProc.java
@@ -0,0 +1,78 @@
+package com.newangels.gen.service.impl.tableToProcedure;
+
+import com.newangels.gen.enums.DataBaseType;
+import com.newangels.gen.exception.UnSupportedDataSourceException;
+import com.newangels.gen.factory.AbstractTableToProcedureFactory;
+import com.newangels.gen.service.AbstractTableToProcedure;
+import com.newangels.gen.service.NameConventService;
+import freemarker.template.Configuration;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 未知数据源表生成存储过程
+ *
+ * @author: TangLiang
+ * @date: 2021/9/13 20:29
+ * @since: 1.0
+ */
+@Service
+public class UnknowTableToProc extends AbstractTableToProcedure {
+
+    @Override
+    protected String getFtlPackageName() {
+        return "unknow";
+    }
+
+    @Override
+    protected void dealGetProcedure(String tableName, List<String> primarys, List<String> primaryTypes, List<String> primaryDesc, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealSelProcedure(String tableName, List<String> selParams, List<String> selParamTypes, List<String> selParamDescs, List<Integer> selType, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealSelWithPageProcedure(String tableName, List<String> selParams, List<String> selParamTypes, List<String> selParamDescs, List<Integer> selType, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealInsProcedure(String tableName, List<String> insParams, List<String> insParamTypes, List<String> insParamDescs, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealUpdProcedure(String tableName, List<String> primarys, List<String> primaryTypes, List<String> primaryDesc, List<String> updParams, List<String> updParamTypes, List<String> updParamDescs, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealSaveProcedure(String tableName, List<String> insParams, List<String> insParamTypes, List<String> insParamDescs, List<String> primarys, List<String> primaryTypes, List<String> primaryDesc, List<String> updParams, List<String> updParamTypes, List<String> updParamDescs, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    protected void dealDelProcedure(String tableName, List<String> primarys, List<String> primaryTypes, List<String> primaryDesc, NameConventService nameConvent, Map<String, Object> objectMap) {
+
+    }
+
+    @Override
+    public Map<String, Object> genProceduresByTable(String tableName, String tableDesc, List<String> params, List<String> paramTypes, List<String> paramDescs, List<Integer> priParamIndex, List<Integer> selParamsIndex, List<Integer> selType, List<Integer> insParamIndex, List<Integer> updParamIndex, NameConventService nameConvent, Configuration configuration) {
+        throw new UnSupportedDataSourceException("未知数据库, 不支持当前数据库生成存储过程");
+    }
+
+    @Override
+    public Map<String, Object> genAutoInsKey(String tableName, String primaryKey, Configuration configuration) {
+        throw new UnSupportedDataSourceException("未知数据库, 不支持当前数据库生成自增主键");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        AbstractTableToProcedureFactory.register(DataBaseType.UNKNOW, this);
+    }
+}
