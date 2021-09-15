e91d3b9bd23d8c1c909f2036bc31bbb59b26f88b
唐亮 <tang97155@163.com>
2021-09-15 09:07:48 +0800

    feat(异步线程池配置):

--- src/main/java/com/newangels/gen/GenApplication.java
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -1,15 +1,23 @@
 package com.newangels.gen;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
+@Slf4j
 public class GenApplication {
+
+    private static final String TIPS = "\n\n" +
+            "******************** 代码生成3.0启动成功 ********************\n";
 
     public static void main(String[] args) {
         SpringApplication.run(GenApplication.class, args);
+        log.info(TIPS);
     }
 
 }
---
+++ src/main/java/com/newangels/gen/config/AsyncConfig.java
@@ -0,0 +1,73 @@
+package com.newangels.gen.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 异步线程池配置
+ *
+ * @author: TangLiang
+ * @date: 2020/7/10 9:17
+ * @since: 1.0
+ */
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    /**
+     * 设置核心线程数
+     */
+    @Value("${thread.corePoolSize}")
+    private int corePoolSize;
+
+    /**
+     * 设置最大线程数
+     */
+    @Value("${thread.maxPoolSize}")
+    private int maxPoolSize;
+
+    /**
+     * 设置队列容量
+     */
+    @Value("${thread.queueCapacity}")
+    private int queueCapacity;
+
+    /**
+     * 设置线程活跃时间（秒）
+     */
+    @Value("${thread.keepAliveSeconds}")
+    private int keepAliveSeconds;
+
+    /**
+     * 设置默认线程名称
+     */
+    @Value("${thread.threadNamePrefix}")
+    private String threadNamePrefix;
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        executor.setThreadNamePrefix(threadNamePrefix);
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+
+}
