86520e3eeb31d7d090e396e11d8dcc875dc5339f
唐亮 <tang97155@163.com>
2021-09-22 09:25:18 +0800

    fix(取消异步线程池配置):
    (cherry picked from commit 3d66f2d0019e8e0df1e5533b272c4ddb98fbe5da)

--- src/main/java/com/newangels/gen/GenApplication.java
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -3,12 +3,11 @@ package com.newangels.gen;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
+//@EnableAsync
 @Slf4j
 public class GenApplication {
 
