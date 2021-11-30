7d010eca3e79c8f2d98ec8ff150569dcf41b0000
唐亮 <tang97155@163.com>
2021-11-30 09:14:48 +0800

    test: 新增lang3包的EventListenerSupport测试方法

---
+++ src/test/java/com/newangels/gen/event/Courier.java
@@ -0,0 +1,22 @@
+package com.newangels.gen.event;
+
+/**
+ * @author: TangLiang
+ * @date: 2021/11/30 9:11
+ * @since: 1.0
+ */
+public class Courier implements IListeners {
+    private String name;
+
+    public Courier(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void receive() {
+        System.out.println("\"" + this.getName() + "\"收到信息： 有快递需要发送");
+    }
+}
---
+++ src/test/java/com/newangels/gen/event/User.java
@@ -0,0 +1,23 @@
+package com.newangels.gen.event;
+
+/**
+ * @author: TangLiang
+ * @date: 2021/11/30 9:11
+ * @since: 1.0
+ */
+public class User implements IListeners {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void receive() {
+        System.out.println("\"" + this.getName() + "\"收到信息：已发货");
+    }
+}
