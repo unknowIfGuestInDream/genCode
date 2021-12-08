b41bf71610eda492814dcbdb6bd937bbc248af6c
唐亮 <tang97155@163.com>
2021-12-08 14:08:14 +0800

    style: 修改启动类中版本号

--- src/main/java/com/newangels/gen/GenApplication.java
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -12,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GenApplication {
 
     private static final String TIPS = "\n\n" +
-            "******************** 代码生成3.0启动成功 ********************\n";
+            "******************** 代码生成3.2启动成功 ********************\n";
 
     public static void main(String[] args) {
         SpringApplication.run(GenApplication.class, args);
