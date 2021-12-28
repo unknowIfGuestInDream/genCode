59f6a8f434e4b81be398bad5fc5c452922d5ec69
唐亮 <tang97155@163.com>
2021-12-28 15:15:57 +0800

    fix: 完善ListToTreeUtil工具类

--- src/main/java/com/newangels/gen/util/ListToTreeUtil.java
+++ src/main/java/com/newangels/gen/util/ListToTreeUtil.java
@@ -1,97 +1,123 @@
 package com.newangels.gen.util;
 
-import java.io.Serializable;
-import java.lang.invoke.SerializedLambda;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import com.newangels.gen.base.BaseUtils;
+
 import java.util.*;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
+ * List转换tree工具类
+ * <p>
+ * 示例
+ * ListToTreeUtil.listToTree(list, (m) -> "-1".equals(m.get("PID")), "ID", "PID", true);
+ * ListToTreeUtil.listToTree(list, (m) -> "-1".equals(m.get("PID")), (m) -> m.get("ID"), (n) -> n.get("PID"), true);
+ *
  * @author: 唐 亮
  * @date: 2021/12/27 23:59
  * @since: 1.0
  */
 public class ListToTreeUtil {
-    @FunctionalInterface
-    public interface XKFunction<T, R> extends Function<T, R>, GetLamadaName {
+    /**
+     * 并发除法阈值，容器大于此值走并发流提高性能
+     */
+    private static int FILL_PALL = 100;
+    /**
+     * 默认是否进行深拷贝，false不进行深拷贝
+     */
+    private static boolean DEFAULT_DEEP = false;
 
-    }
+    /**
+     * list转换为tree结构数据
+     *
+     * @param list      数据
+     * @param isRoot    判断为根节点的函数
+     * @param idFun     id函数
+     * @param pidFun    上级id函数
+     * @param deepClone 是否进行深克隆，默认为否
+     */
+    public static List<Map<String, Object>> listToTree(List<Map<String, Object>> list, Predicate<Map<String, Object>> isRoot, Function<Map<String, Object>, ?> idFun, Function<Map<String, Object>, ?> pidFun, boolean deepClone) {
+        if (Objects.isNull(list) || Objects.isNull(idFun) || Objects.isNull(pidFun)) {
+            return new ArrayList<>();
+        }
+        List<Map<String, Object>> cloneList = list;
+        if (deepClone) {
+            cloneList = (List<Map<String, Object>>) BaseUtils.deepClone(list);
+        }
+        //方法复杂度为O(n2)数据量超过一百条时走并发流优化性能
+        List<Map<String, Object>> children;
+        if (list.size() > FILL_PALL) {
+            children = Collections.synchronizedList(new ArrayList<>());
+            List<Map<String, Object>> finalCloneList = Collections.synchronizedList(cloneList);
+            cloneList.parallelStream()
+                    .filter(isRoot).forEachOrdered(s -> {
+                children.add(s);
+                fillChild(s, finalCloneList, idFun, pidFun);
+            });
+        } else {
+            children = new ArrayList<>();
+            for (Map<String, Object> child : cloneList) {
+                if (isRoot.test(child)) {
+                    children.add(child);
+                    fillChild(child, cloneList, idFun, pidFun);
+                }
+            }
+        }
 
-    @FunctionalInterface
-    public interface XKBiConsumer<T, R> extends BiConsumer<T, R>, GetLamadaName {
-
-    }
-
-    @FunctionalInterface
-    public interface XKPredicate<T> extends Predicate<T>, GetLamadaName {
-
+        return children;
     }
 
     /**
-     * System.out.println(isRoot.getLamadaMethodName());
+     * list转换为tree结构数据
+     *
+     * @param list      数据
+     * @param isRoot    判断为根节点的函数
+     * @param idField   id字段
+     * @param pidField  上级id字段
+     * @param deepClone 是否进行深克隆，默认为否
      */
-    public interface GetLamadaName extends Serializable {
-        String METHOD_NAME = "writeReplace";
-
-        //todo  可用map做个缓存，但只能在循环时好用
-        default String getLamadaMethodName() {
-            final Class<? extends GetLamadaName> aClass = this.getClass();
-            String implMethodName = null;
-            try {
-                final Method method = aClass.getDeclaredMethod(METHOD_NAME);
-                method.setAccessible(true);
-                SerializedLambda lamada = (SerializedLambda) method.invoke(this);
-                implMethodName = lamada.getImplMethodName();
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-            return implMethodName;
-        }
+    public static List<Map<String, Object>> listToTree(List<Map<String, Object>> list, Predicate<Map<String, Object>> isRoot, String idField, String pidField, boolean deepClone) {
+        return listToTree(list, isRoot, (m) -> m.get(idField), (n) -> n.get(pidField), deepClone);
     }
 
-    public static <T> List<T> ListToTree(List<T> source, XKPredicate<T> isRoot, XKFunction<T, ?> idFun, XKFunction<T, ?> pidFun, XKFunction<T, List<T>> getChildFun, XKBiConsumer<T, List<T>> setChildFun) {
-        if (Objects.isNull(source) || Objects.isNull(idFun) || Objects.isNull(pidFun) || Objects.isNull(getChildFun)) {
-            return new ArrayList<>();
-        }
-        List<T> ret = new ArrayList<>();
-        Map<Object, T> map = new HashMap<>();
-        source.forEach(t -> {
-            Optional.ofNullable(isRoot).map(r -> {
-                if (isRoot.test(t)) {
-                    ret.add(t);
-                }
-                return r;
-            }).orElseGet(() -> {
-                Optional.ofNullable(pidFun.apply(t)).orElseGet(() -> {
-                    ret.add(t);
-                    return null;
-                });
-                return null;
+    public static List<Map<String, Object>> listToTree(List<Map<String, Object>> list, Predicate<Map<String, Object>> isRoot, String idField, String pidField) {
+        return listToTree(list, isRoot, idField, pidField, DEFAULT_DEEP);
+    }
+
+    public static List<Map<String, Object>> listToTree(List<Map<String, Object>> list, Predicate<Map<String, Object>> isRoot, Function<Map<String, Object>, ?> idFun, Function<Map<String, Object>, ?> pidFun) {
+        return listToTree(list, isRoot, idFun, pidFun, DEFAULT_DEEP);
+    }
+
+    /**
+     * 递归生成树节点
+     *
+     * @param child    每个元素
+     * @param list     数据集
+     * @param idField  id字段
+     * @param pidField 上级id字段
+     */
+    private static void fillChild(Map<String, Object> child, List<Map<String, Object>> list, String idField, String pidField) {
+        fillChild(child, list, (m) -> m.get(idField), (n) -> n.get(pidField));
+    }
+
+    private static void fillChild(Map<String, Object> child, List<Map<String, Object>> list, Function<Map<String, Object>, ?> idFun, Function<Map<String, Object>, ?> pidFun) {
+        List<Map<String, Object>> children;
+        if (list.size() > FILL_PALL) {
+            children = Collections.synchronizedList(new ArrayList<>());
+            list.parallelStream().filter(s -> idFun.apply(child).equals(pidFun.apply(s))).forEachOrdered(s -> {
+                children.add(s);
+                fillChild(s, list, idFun, pidFun);
             });
-            map.put(idFun.apply(t), t);
-        });
-        source.forEach(t -> map.computeIfPresent(pidFun.apply(t), (k, v) -> {
-            Optional.ofNullable(getChildFun.apply(v)).orElseGet(() -> {
-                List<T> list = new ArrayList<>();
-                setChildFun.accept(v, list);
-                return list;
-            }).add(t);
-            return v;
-        }));
-        return ret;
-    }
-
-    public static <T> List<T> ListToTree(List<T> source, XKFunction<T, ?> idFun, XKFunction<T, ?> pidFun, XKFunction<T, List<T>> getChildFun, XKBiConsumer<T, List<T>> setChildFun) {
-        return ListToTree(source, null, idFun, pidFun, getChildFun, setChildFun);
-    }
-
-    private static <T> String getMethodName(GetLamadaName fun) {
-        if (fun != null) {
-            fun.getLamadaMethodName();
+        } else {
+            children = new ArrayList<>();
+            for (Map<String, Object> childDiv : list) {
+                if (idFun.apply(child).equals(pidFun.apply(childDiv))) {
+                    children.add(childDiv);
+                    fillChild(childDiv, list, idFun, pidFun);
+                }
+            }
         }
-        return null;
+
+        child.put("children", children);
     }
 }
--- src/test/java/com/newangels/gen/util/ListToTreeTest.java
+++ src/test/java/com/newangels/gen/util/ListToTreeTest.java
@@ -2,15 +2,14 @@ package com.newangels.gen.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-import lombok.experimental.Accessors;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: 唐 亮
@@ -21,39 +20,35 @@ import java.util.List;
 public class ListToTreeTest {
     @Autowired
     private ObjectMapper objectMapper;
-    public static List<Node> nodeList;
-
-    @Data
-    @Accessors(chain = true)
-    public static class Node {
-        Integer id;
-        Integer pid;
-        List<Node> children;
-    }
 
     @Test
     public void listToTree() throws JsonProcessingException {
-        List<Node> nodes = ListToTreeUtil.ListToTree(nodeList, Node::getId, Node::getPid, Node::getChildren, Node::setChildren);
-        //List<Node> nodes = ListToTreeUtil.ListToTree(nodeList, (n) -> n.getPid() == null || n.getPid().equals(0), Node::getId, Node::getPid, Node::getChildren, Node::setChildren);
-        System.out.println(objectMapper.writeValueAsString(nodes));
+        List<Map<String, Object>> list = new ArrayList<>();
+        init(list);
+        List<Map<String, Object>> children = ListToTreeUtil.listToTree(list, (m) -> "-1".equals(m.get("PID")), "ID", "PID", true);
+        List<Map<String, Object>> children1 = ListToTreeUtil.listToTree(list, (m) -> "-1".equals(m.get("PID")), (m) -> m.get("ID"), (n) -> n.get("PID"), true);
+        System.out.println(objectMapper.writeValueAsString(list));
+        System.out.println(objectMapper.writeValueAsString(children));
+        System.out.println(objectMapper.writeValueAsString(children1));
     }
 
-    @BeforeAll
-    public static void before() {
-        nodeList = new ArrayList<>();
-        nodeList.add(new Node().setId(0));
-        nodeList.add(new Node().setId(1));
-        nodeList.add(new Node().setId(2).setPid(0));
-        nodeList.add(new Node().setId(3).setPid(0));
-        nodeList.add(new Node().setId(4).setPid(1));
-        nodeList.add(new Node().setId(5).setPid(1));
-        nodeList.add(new Node().setId(6).setPid(2));
-        nodeList.add(new Node().setId(7).setPid(2));
-        nodeList.add(new Node().setId(8).setPid(3));
-        nodeList.add(new Node().setId(9).setPid(3));
-        nodeList.add(new Node().setId(10).setPid(4));
-        nodeList.add(new Node().setId(11).setPid(4));
-        nodeList.add(new Node().setId(12).setPid(5));
+    public void init(List<Map<String, Object>> list) {
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("ID", "1");
+        map1.put("PID", "-1");
+        list.add(map1);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("ID", "2");
+        map2.put("PID", "1");
+        list.add(map2);
+        Map<String, Object> map3 = new HashMap<>();
+        map3.put("ID", "3");
+        map3.put("PID", "2");
+        list.add(map3);
+        Map<String, Object> ma4 = new HashMap<>();
+        ma4.put("ID", "4");
+        ma4.put("PID", "2");
+        list.add(ma4);
     }
 
 }
