0f5bd2f6b4ab493806e90b4dab52ac8bd7bb5205
唐亮 <tang97155@163.com>
2022-01-05 17:24:13 +0800

    build: 修改启动信息

--- src/main/java/com/newangels/gen/GenApplication.java
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -1,6 +1,7 @@
 package com.newangels.gen;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GenApplication {
 
     private static final String TIPS = "\n\n" +
-            "******************** 代码生成3.2启动成功 ********************\n";
+            "******************** 代码生成系统启动成功 ********************\n";
 
     public static void main(String[] args) {
         SpringApplication.run(GenApplication.class, args);
