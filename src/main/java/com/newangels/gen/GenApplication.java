e67b307abbb5c5bcdadf4c13b03582cc3d8af381
唐亮 <tang97155@163.com>
2022-01-22 18:01:48 +0800

    fix: 优化导入

--- src/main/java/com/newangels/gen/GenApplication.java
+++ src/main/java/com/newangels/gen/GenApplication.java
@@ -1,7 +1,6 @@
 package com.newangels.gen;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
